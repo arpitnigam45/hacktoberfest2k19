8c5aa2363893708c65b31ca819a82f222bce2d85
Arpit Nigam <43514844+arpitnigam45@users.noreply.github.com>
2019-10-20 02:26:23 +0530

    Update code.py

--- ListOps/code.py
+++ ListOps/code.py
@@ -9,10 +9,13 @@ Example: given list -> [1,2,3,4,5]
 
 '''
 
-nums = [3, 5, 2, 9, 101, 333, 4]
 
+nums = [3, 5, 2, 9, 101, 333, 4]
 while len(nums):
-    for i in range(len(nums)):
+    if(len(nums)==1):
+        del(nums[0])
+        break
+    for i in range(len(nums)-1):
         if i%2==0:
             del nums[i]
     print(nums)
