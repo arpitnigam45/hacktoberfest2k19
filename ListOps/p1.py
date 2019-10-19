d9e3d0b19a18e570fe46146cd4e4c8507af81686
Arpit Nigam <43514844+arpitnigam45@users.noreply.github.com>
2019-10-20 02:41:12 +0530

    Update p1.py

--- ListOps/p1.py
+++ ListOps/p1.py
@@ -1,3 +1,13 @@
+'''
+Task: Delete alternate elements of the given list, starting from index 0,
+      print the list after each traversal.
+
+Example: given list -> [1,2,3,4,5]
+                o/p: [2, 4]
+                     [4]
+                     []
+
+'''
 
 nums = [3, 5, 2, 9, 101, 333, 4]
 while len(nums):
@@ -8,3 +18,4 @@ while len(nums):
         if i%2==0:
             del nums[i]
     print(nums)
+print(nums)
